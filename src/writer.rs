d6e0476b3a1e35fe102c1a3eeae1094f7daf3f56
clarkenciel <clarkenciel@gmail.com>
2017-11-11 16:42:32 -0800

    feat(writer): wave write command works

--- src/writer.rs
+++ src/writer.rs
@@ -1,3 +1,13 @@
-pub fn write(file_name: &str, duration: f32, expression: &str) -> Result<(),String> {
-    Ok(())
+use bbb_core::signal::ExprSignal;
+use bbb_core::parser::parse;
+use bbb_core::wav::Recorder;
+
+type WriteResult<'a> = Result<&'a str, String>;
+
+pub fn write<'a>(file_name: &'a str, duration: f32, expression: &'a str) -> WriteResult<'a> {
+    parse(expression)
+        .and_then(|expr| {
+            Recorder::new(8_000).record(file_name, duration, &mut ExprSignal::from(expr))
+        })
+        .and(Ok(file_name))
 }
